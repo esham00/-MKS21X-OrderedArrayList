efaa163fe7b7168a35a4c1d883ba1dae4947ee94
Elizabeth Sham <esham00@stuy.edu>
2018-11-01 21:28:50 -0400

    Problem w set variable T element

--- Driver.java
+++ Driver.java
@@ -1,5 +1,41 @@
  public class Driver {
     public static void main(String[] args){
         NoNullArrayList<String> a = new NoNullArrayList<String>();
+	NoNullArrayList<Integer> b = new NoNullArrayList<Integer>();
+	a.add("a");
+	a.add("b");
+	a.add("c");
+	a.add("d");
+	b.add(2);
+	b.add(0, new Integer (1));
+	System.out.println(a);
+	System.out.println(b);
+	try {
+	    b.add(null);
+	}
+	catch (IllegalArgumentException e) {
+	    e.printStackTrace();
+	}
+	try {
+	    a.add(null);
+	}
+	catch(IllegalArgumentException e) {
+	    e.printStackTrace();
+	}
+	try {
+	    b.add(1, null);
+	}
+	catch (IllegalArgumentException e) {
+	    e.printStackTrace();
+	}
+	try {
+	    a.add(1, null);
+	}
+	catch (IllegalArgumentException e) {
+	    e.printStackTrace();
+	}
+	System.out.println(a);
+	System.out.println(b);
     }
  }
+		
--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -6,13 +6,13 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     public NoNullArrayList(int startingCapacity) {
 	super(startingCapacity);
     }
-    public void set(T element) {
-	if (element == null) {
-	    throw new IllegalArgumentException();
-	}
-	else {
-	    super.set(element);
-	}
+    public String set(int index, T element) {
+    	if (element == null) {
+    	    throw new IllegalArgumentException();
+    	}
+    	else {
+   	    super.set(index, element);
+    	}
     }
     public boolean add(T element) {
 	if (element == null) {
@@ -20,6 +20,15 @@ public class NoNullArrayList<T> extends ArrayList<T> {
 	}
 	else {
 	    super.add(element);
+	    return true;
+	}
+    }
+    public void add(int index, T element) {
+	if (element == null) {
+	    throw new IllegalArgumentException();
+	}
+	else {
+	    super.add(index, element);
 	}
     }
 }
