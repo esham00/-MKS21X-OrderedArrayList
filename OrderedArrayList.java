7c9b975f1d32ded31d385a08d01eddb809c80dda
Elizabeth Sham <esham00@stuy.edu>
2018-11-02 19:00:22 -0400

    Shorter order now, works quicker

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,47 +5,29 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     public OrderedArrayList(int startingCapacity) {
 	super(startingCapacity);
     }
-    private void order() {
-	T check = this.get(0);
-	int all = 0;
-	int size = this.size();
-	int same = 0;
-	NoNullArrayList<T> xin = new NoNullArrayList<T>(this.size());
-	while (size > 1) {
-	    for (int i = 0; i < size; i++) {
-		if (check.compareTo(this.get(i)) < 0) {
-		    all++;
-		}
-		if (check.compareTo(this.get(i)) == 0 & i != 0) {
-		    same++;
-		}
+    private int order(T element) {
+	int index = 0;
+        for (int i = 0; i < this.size(); i++) {
+	    if (element.compareTo(this.get(i)) >= 0) {
+		index = i + 1;
 	    }
-	    if (all == size-1) {
-		xin.add(check);
-		this.remove(0);
-		check = this.get(0);
-		size--;
-	    }
-	    else {
-		T a = check;
-		this.remove(0);
-		super.add(check);
-		check = this.get(0);
-	    }
-	}
-	xin.add(this.get(0));
-	this.set(0, xin.get(0));
-	for (int i = 1; i < xin.size(); i++) {
-	    super.add(xin.get(i));
-	}
+    	}
+	return index;
     }		
     public boolean add(T element) {
-	super.add(element);
-	order();
+	super.add(order(element), element);
 	return true;
     }
     public void add(int index, T element) {
-	super.add(index, element);
-	order();
+    	super.add(order(element), element);
+    }
+    public T set(int index, T element) {
+	if (index > this.size() || index < 0) {
+	    throw new IllegalArgumentException();
+	}
+	T prevEle = this.get(index);
+	this.remove(index);
+	this.add(element);
+	return prevEle;
     }
 }
