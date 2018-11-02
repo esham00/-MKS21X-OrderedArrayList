7298a0d47ff3561d8cc70cdfc32e0d579069d167
Elizabeth Sham <esham00@stuy.edu>
2018-11-02 18:39:20 -0400

    Works but takes a LONG time to load

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,18 +5,47 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     public OrderedArrayList(int startingCapacity) {
 	super(startingCapacity);
     }
-    private OrderedArrayList<T> order(OrderedArrayList<T> da) {
-	for (int i = 0; i < da.size()-1; i++) {
-	    if (T == String) {
-		if (da.get(i).charAt(0) > da.get(i+1).charAt(0)) {
-		    String a = da.get(i+1);
-		    da.set(i+1, da.get(i));
-		    da.set(i, a);
+    private void order() {
+	T check = this.get(0);
+	int all = 0;
+	int size = this.size();
+	int same = 0;
+	NoNullArrayList<T> xin = new NoNullArrayList<T>(this.size());
+	while (size > 1) {
+	    for (int i = 0; i < size; i++) {
+		if (check.compareTo(this.get(i)) < 0) {
+		    all++;
+		}
+		if (check.compareTo(this.get(i)) == 0 & i != 0) {
+		    same++;
 		}
 	    }
+	    if (all == size-1) {
+		xin.add(check);
+		this.remove(0);
+		check = this.get(0);
+		size--;
+	    }
 	    else {
-		if (da.get
-		
+		T a = check;
+		this.remove(0);
+		super.add(check);
+		check = this.get(0);
+	    }
+	}
+	xin.add(this.get(0));
+	this.set(0, xin.get(0));
+	for (int i = 1; i < xin.size(); i++) {
+	    super.add(xin.get(i));
+	}
+    }		
     public boolean add(T element) {
+	super.add(element);
+	order();
+	return true;
+    }
+    public void add(int index, T element) {
+	super.add(index, element);
+	order();
     }
 }
