b7eb7e0a8602adb4b9adafa34253a70b92d48389
Elizabeth Sham <esham00@stuy.edu>
2018-11-02 11:01:28 -0400

    classwork

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,7 +8,7 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     private OrderedArrayList<T> order(OrderedArrayList<T> da) {
 	for (int i = 0; i < da.size()-1; i++) {
 	    if (T == String) {
-		if (da.get(i).charAt(i) > da.charAt(i+1)) {
+		if (da.get(i).charAt(0) > da.get(i+1).charAt(0)) {
 		    String a = da.get(i+1);
 		    da.set(i+1, da.get(i));
 		    da.set(i, a);
